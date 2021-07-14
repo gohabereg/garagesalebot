3083781dbf94600f4f629fbb1d6386e9e737f9a6
Georgy Berezhnoy <gohabereg@gmail.com>
2021-07-14 22:12:18 +0300

    Initial commit

---
+++ garagesalebot/bot.py
@@ -0,0 +1,20 @@
+from os import getenv
+from telegram.ext import Updater, MessageHandler, Filters, Dispatcher
+from telegram import Update
+from telegram.ext.callbackcontext import CallbackContext
+
+
+class Bot: 
+    def __init__(self):
+        self.updater: Updater = Updater(token=getenv('BOT_TOKEN'))
+        self.dispatcher: Dispatcher = self.updater.dispatcher
+        
+        self.dispatcher.add_handler(MessageHandler(Filters.text, self.echo))
+
+        self.updater.start_polling()
+
+        print('Bot started')
+
+    def echo(self, update: Update, context: CallbackContext) -> None:
+        update.message.reply_text(update.message.text)
+
---
+++ main.py
@@ -0,0 +1,6 @@
+from dotenv import load_dotenv
+from garagesalebot.bot import Bot
+
+load_dotenv()
+
+bot = Bot()
